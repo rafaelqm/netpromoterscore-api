4ed11b25a8bfb4a7d28b1d1194b0b0ef14947741
Rafael Querino Moreira <rafaelqm@gmail.com>
2021-04-24 17:17:31 -0300

    Created database connection and user model

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import "./database"
+import { router } from "./routes";
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => console.log("Server running"));
